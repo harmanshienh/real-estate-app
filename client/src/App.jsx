5da9adefe6913d1877e1f8b8099db4a0a38dc5bf
Harman <harmanshienh01@gmail.com>
2024-01-31 21:23:31 -0500

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from "./components/Header"
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
