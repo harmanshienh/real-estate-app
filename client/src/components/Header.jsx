4a8f76b2c3d1781f1319cb360618f3a22c2ac776
Harman <harmanshienh01@gmail.com>
2024-05-10 22:46:38 -0400

    Create Sign-in page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
-        <header className='bg-slate-200 shadow-md'>
+        <header className='bg-yellow-500 shadow-md'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
                 <Link to='/'>
                     <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                        <span className='text-slate-500'>Harman</span>
-                        <span className='text-slate-700'>Estate</span>
+                        <span className='text-slate-500 mr-2'>Waterloo</span>
+                        <span className='text-slate-700'>Student Housing</span>
                     </h1>
                 </Link>
                 <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
